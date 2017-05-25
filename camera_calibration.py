f1fe97aba1504721db57b4693a66d1eab0386c05
bkamen <boris.kamenjasevic@googlemail.com>
2017-05-25 23:37:37 +0200

    added function to create undistorted images of a images in one folder

--- camera_calibration.py
+++ camera_calibration.py
@@ -11,7 +11,7 @@ def calibrate_camera(folder, nx, ny, save_result=1, save_log=1):
 
     # initialise arrays
     objp = np.zeros((ny * nx, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)  # objectpoints for each image
+    objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)  # object points for each image
     objpts = []  # empty array for all object points
     imgpts = []  # empty array for all image points
     cal_successful = np.zeros(len(im_list))  # array for check if detecting corners was successful, needed for log file
@@ -31,11 +31,11 @@ def calibrate_camera(folder, nx, ny, save_result=1, save_log=1):
             imgpts.append(corners)
 
             # Uncomment block to see images with corners drawn on
-            #im = cv2.drawChessboardCorners(im, (nx, ny), corners, ret)
-            #cv2.imshow('img', im)
-            #cv2.waitKey(500)
+            # im = cv2.drawChessboardCorners(im, (nx, ny), corners, ret)
+            # cv2.imshow('img', im)
+            # cv2.waitKey(500)
 
-    # camer calibration function
+    # camera calibration function
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpts, imgpts, gray.shape[::-1], None, None)
 
     # if not elsewise specified save camera calibration output
@@ -77,5 +77,27 @@ def corner_detection(img, nx, ny):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # find the chessboard corners, number of corners passed as parameters
     ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
     return ret, corners
+
+
+def undistort_images(folder, mtx, dist, nx, ny, chessboarddrawn=1):
+    # list of images that should be processed
+    im_list = glob.glob(folder + '*.jpg')
+
+    # loop through image list
+    for i in range(len(im_list)):
+        img = cv2.imread(im_list[i])
+        # if user specified that chess board corners should be drawn, the saved file will have it when successfully
+        # detected
+        if chessboarddrawn:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+            if ret:
+                img = cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+        # function to undistort image with output of camera calibration
+        dst = cv2.undistort(img, mtx, dist, None, mtx)
+        # define the filename for the output
+        filename = './output_images/undistorted_'+im_list[i].split('\\')[-1]
+        # create the file
+        cv2.imwrite(filename, dst)
+    return None
