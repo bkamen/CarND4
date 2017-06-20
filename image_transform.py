822ba9dd4f4394daa29802dd1b5ead8aa19b807c
bkamen <boris.kamenjasevic@googlemail.com>
2017-06-20 20:32:33 +0200

    adjusted lateral offset

--- image_transform.py
+++ image_transform.py
@@ -86,7 +86,7 @@ def persp_transform(img):
     # function that performs perspective transform
     # source and destination are hardcoded and are found empirically by testing and deciding subjectively
     imshape = (img.shape[1], img.shape[0])
-    offset = 0
+    offset = -100
     dst = np.float32([(450+offset, 0), (200+offset, imshape[1]), (imshape[0]-450+offset, 0), (imshape[0]-200+offset, imshape[1])])
     src = np.float32([(605, 470), (180, imshape[1]), (imshape[0] - 605, 470), (imshape[0] - 180, imshape[1])])
     #src = np.float32([[490, 482], [810, 482], [1250, 720], [40, 720]])
