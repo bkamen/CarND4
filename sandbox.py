f04ad109813addc0f192c65c54c45fe82850f8f9
bkamen <boris.kamenjasevic@googlemail.com>
2017-06-22 23:02:13 +0200

    playing around

--- sandbox.py
+++ sandbox.py
@@ -2,14 +2,33 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = './test_images/straight_lines1.jpg'
+file = './test_images/test5.jpg'
+
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
 
 img = cv2.imread(file)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-img = clahe.apply(img)
+clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(4, 4))
+#img = clahe.apply(img)
 
-plt.imshow(img, cmap='gray')
-plt.show()
+img = adjust_gamma(img, .1)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img = clahe.apply(img)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+
+#plt.imshow(adjust_gamma(img))
+#plt.show()
+
 
