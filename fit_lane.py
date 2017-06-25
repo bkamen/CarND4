cc4b3e28e3772ff0d646361d9ae712c8f4972c36
bkamen <boris.kamenjasevic@googlemail.com>
2017-06-25 22:28:11 +0200

    corrected scaling factors

--- fit_lane.py
+++ fit_lane.py
@@ -168,8 +168,8 @@ def meas_curv(llane, rlane):
     y_eval = np.max(ploty)
 
     # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30 / 720  # meters per pixel in y dimension
-    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+    ym_per_pix = 40 / 720  # meters per pixel in y dimension
+    xm_per_pix = 4 / 1080  # meters per pixel in x dimension
 
     # Fit new polynomials to x,y in world space
     left_fit_cr = np.polyfit(ploty * ym_per_pix, leftx * xm_per_pix, 2)
